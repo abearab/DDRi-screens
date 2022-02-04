e6726b6b0d1ab7e3f3452bcdb599e026b7c8618a
abearab <abardesigner@gmail.com>
2022-02-03 22:53:36 -0800

    backup

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import numpy as np
 from operator import itemgetter
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+from scipy.spatial.distance import squareform
 
 
 def find_top(df,value, value_thr, stat, stat_thr, drop_dup=False,silent=False):
@@ -33,25 +35,35 @@ def find_top(df,value, value_thr, stat, stat_thr, drop_dup=False,silent=False):
     return up, dn
 
 
-def get_score(screen,score,rename=None,pseudo=False):
-    out = screen['gene scores'].xs(score, level=0, axis=1).xs('ave_rep1_rep2', level=0, axis=1)
-    del out['transcripts']
-    
-    if rename:
-        score = rename
-    
-    out = out.loc[:,['average phenotype of strongest 3','Mann-Whitney p-value']].rename({
+def get_score(screen,score,rename=None,rep='ave_rep1_rep2'):
+    df = screen['gene scores'].xs(score, level=0, axis=1).xs(rep, level=0, axis=1)
+    del df['transcripts']
+    if rename: score = rename
+    df = df.loc[:,['average phenotype of strongest 3','Mann-Whitney p-value']].rename({
         'average phenotype of strongest 3': score + '.rho', 
         'Mann-Whitney p-value': score+'.pvalue'
     }, axis='columns')
     
-    if not pseudo: 
-        out = out.loc[['pseudo' not in g for g in out.index],:]
+    ### Normalization based on Negative Control gRNAs ### 
+    ## step 1: select neg ctrl gRNAs scores
+    ctrl_gRNA = np.array(['pseudo' in g for g in df.index])     
+    ## step 2: measure mean neg ctrl gRNAs scores
+    mean_ctrl_gRNA  = np.median(df.loc[ctrl_gRNA,score + '.rho'])
+    ## step 3: subtract mean of neg ctrl gRNAs scores
+    df.loc[:,score + '.rho.norm'] = df.loc[:,score + '.rho'] - mean_ctrl_gRNA
+    ## step 4: measure std of neg ctrl gRNAs scores
+    sigma = np.std(df.loc[ctrl_gRNA,score + '.rho'])
+    ## step 5: divide by std of neg ctrl gRNAs scores
+    df.loc[:,score + '.rho.norm'] = df.loc[:,score + '.rho.norm'] / sigma
+    ## step 6: remove rows with pseudo index, neg control gRNAs 
+    df = df.loc[ctrl_gRNA == False,:]
     
-    return out
+    print (f'{score} ->\n\tmean(neg control gRNAs rho score): {mean_ctrl_gRNA}\n\tstd(neg control gRNAs rho score): {sigma}')
+    
+    return df
 
 
-# def plot_corr(df,title,vmin=None,vmax=None,sub=111):
+#     def plot_corr(df,title,vmin=None,vmax=None,sub=111):
 #     fig = plt.figure()
 #     ax = fig.add_subplot(sub)
 
@@ -69,15 +81,48 @@ def get_score(screen,score,rename=None,pseudo=False):
 
 
 #     plt.show()
-def plot_corr(df,ax,vmin=None,vmax=None):
+
+
+
+def corr_clust(data,dendrogram=False, threshold = 0.8):
+    '''Correlation Heatmaps with Hierarchical Clustering
+    '''
+    # https://www.kaggle.com/sgalella/correlation-heatmaps-with-hierarchical-clustering
+    dissimilarity = 1 - abs(data.corr())
+    Z = linkage(squareform(dissimilarity), 'complete')
+    
+    if dendrogram:
+        return Z
+        
+    else:
+        # the cluster
+        labels = fcluster(Z, threshold, criterion='distance')
+
+        # Keep the indices to sort labels
+        labels_order = np.argsort(labels)
+
+        # Build a new dataframe with the sorted columns
+        for idx, i in enumerate(data.columns[labels_order]):
+            if idx == 0:
+                clustered = pd.DataFrame(data[i])
+            else:
+                df_to_append = pd.DataFrame(data[i])
+                clustered = pd.concat([clustered, df_to_append], axis=1)
+
+        correlations = clustered.corr()
+
+        return correlations
+        
+
+def plot_corr(data,ax,vmin=None,vmax=None):
     # Compute the correlation matrix
-    corr = df.corr('spearman')
+    corr = corr_clust(data)
     # Generate a mask for the upper triangle
     mask = np.triu(np.ones_like(corr, dtype=bool))
     # Generate a custom diverging colormap
     cmap = sns.diverging_palette(230, 20, as_cmap=True)
     # Draw the heatmap with the mask and correct aspect ratio
     sns.heatmap(
-        corr, mask=mask, cmap=cmap, vmin=vmin,vmax=vmax, center=0,
+        round(corr,2), mask=mask, cmap=cmap, vmin=vmin,vmax=vmax, center=0,
         square=True, linewidths=.5, cbar_kws={"shrink": .5},ax=ax
     )
