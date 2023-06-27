41d2de5cfba22da6e1be77032220e91bfb9772a7
abearab <abardesigner@gmail.com>
2023-06-27 16:37:40 -0700

    update `phenoScore`

--- CRISPRi-PRDX1-screens/scripts/phenoScore.py
+++ CRISPRi-PRDX1-screens/scripts/phenoScore.py
@@ -16,13 +16,13 @@ def run_deseq2_norm(adata):
 
     
 def getDelta(x,y):
-    """log ratio of y / x
+    """log ratio of y / x, averaged across replicates 
     """
     return np.mean(np.log1p(y) - np.log1p(x),axis=1)
 
 
-def getGrowthScore(x,y,x_ctrl,y_ctrl,growth_rate):
-    """
+def getScore(x,y,x_ctrl,y_ctrl,growth_rate):
+    """g
     """
     ctrl_std = np.std(getDelta(x_ctrl,y_ctrl))
     ctrl_median = np.median(getDelta(x_ctrl,y_ctrl))
@@ -30,8 +30,8 @@ def getGrowthScore(x,y,x_ctrl,y_ctrl,growth_rate):
     return ((getDelta(x,y) - ctrl_median) / growth_rate) / ctrl_std
 
 
-def runPhenoScore(adata,cond1,cond2,growth_rate=1,n_reps=2):
-    # prep counts
+def runPhenoScore(adata,cond1,cond2,growth_rate=1,n_reps=2,test='ttest'):
+    ## prep counts
     df_cond1 = adata[adata.obs.query(f'condition=="{cond1}"').index[:n_reps],].to_df('deseq').T
     df_cond2 = adata[adata.obs.query(f'condition=="{cond2}"').index[:n_reps],].to_df('deseq').T
 
@@ -41,18 +41,22 @@ def runPhenoScore(adata,cond1,cond2,growth_rate=1,n_reps=2):
     x_ctrl=df_cond1[adata.var.targetType.eq('negCtrl')].to_numpy()
     y_ctrl=df_cond2[adata.var.targetType.eq('negCtrl')].to_numpy()
     
-    # 
-    phenotype_score = getGrowthScore(x,y,x_ctrl,y_ctrl,growth_rate)
+    ## calculate growth score
+    phenotype_score = getScore(x,y,x_ctrl,y_ctrl,growth_rate)
     
     adata.var[f'condition_{cond2}_vs_{cond1}_delta'] = phenotype_score
     
-    # run Mann-Whitney U rank test on replicates
-    
-    # run ttest on replicates
-    pvalues = ttest_rel(y,x, axis=1)[1]
-    adata.var[f'condition_{cond2}_vs_{cond1}_pvalue'] = pvalues
+    ## calculate p-values 
+    if test=='MW':
+        # run Mann-Whitney U rank test on replicates
+        pass
+    if test=='ttest':
+        # run ttest on replicates
+        pvalues = ttest_rel(y,x, axis=1)[1]
+        adata.var[f'condition_{cond2}_vs_{cond1}_pvalue'] = pvalues
 
-    # # Calculate the adjusted p-values using the Benjamini-Hochberg method
+    ## calculate FDR
+    # Calculate the adjusted p-values using the Benjamini-Hochberg method
     # _, adj_pvalues, _, _ = multipletests(adata.var[f'condition_{cond1}_vs_{cond2}_pvalue'], alpha=0.05, method='fdr_bh')
     # adata.var[f'condition_{cond1}_vs_{cond2}_adj_pvalue'] = adj_pvalues
     
